501a4e80aa66fefc38c6b574aff316ffcbb44076
SilasMahler <simah002@stud.fh-dortmund.de>
2018-01-23 11:44:46 +0100

    minified restcontroller

--- test-service/src/main/kotlin/de/adesso/testservice/TestServiceApplication.kt
+++ test-service/src/main/kotlin/de/adesso/testservice/TestServiceApplication.kt
@@ -1,10 +1,8 @@
 package de.adesso.testservice
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicLong
@@ -17,37 +15,11 @@ fun main(args: Array<String>) {
     runApplication<TestServiceApplication>(*args)
 }
 
-data class Greeting(val id: Long, val content: String)
-
 @RestController
 class GreetingsController {
     val counter = AtomicLong()
 
     @GetMapping("/greeting")
-    fun greeting(@RequestParam(value= "name", defaultValue = "World")
-                 name: String)
-    = Greeting(counter.incrementAndGet(), "Hello, $name")
-}
-
-@RestController
-@RequestMapping(value = "/test")
-class DataController {
-
-    @Autowired
-    lateinit var dataService: DataService
-
-    @RequestMapping(method = arrayOf(RequestMethod.GET))
-    fun getAll() = dataService.getAll()
-
-}
-
-@Service
-class DataService {
-    val counter = AtomicLong()
-
-    fun getAll(): Array<String> {
-        println("call")
-        return arrayOf(counter.incrementAndGet().toString(),"Hello", "from", "spring", "service", "now", "it","is", LocalDateTime.now().toString())
-    }
-
+    fun greeting()
+            = arrayOf(counter.incrementAndGet(), "Hello", "from", "spring", "service", "2", "now", "it", "is", LocalDateTime.now().toString())
 }
--- test-service2/src/main/kotlin/de/adesso/testservice2/TestServiceApplication.kt
+++ test-service2/src/main/kotlin/de/adesso/testservice2/TestServiceApplication.kt
@@ -2,12 +2,23 @@ package de.adesso.testservice2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.util.concurrent.atomic.AtomicLong
 
 @SpringBootApplication
-@EnableDiscoveryClient
 class TestServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<TestServiceApplication>(*args)
+}
+
+@RestController
+class GreetingsController {
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting()
+            = arrayOf(counter.incrementAndGet(), "Hello", "from", "spring", "service", "2", "now", "it","is", LocalDateTime.now().toString())
 }
