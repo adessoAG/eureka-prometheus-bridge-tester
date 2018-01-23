0c427ca9d9c87558b6abf8a38f76fe1069aefec4
SilasMahler <simah002@stud.fh-dortmund.de>
2018-01-23 11:45:09 +0100

    correct typo

--- test-service/src/main/kotlin/de/adesso/testservice/TestServiceApplication.kt
+++ test-service/src/main/kotlin/de/adesso/testservice/TestServiceApplication.kt
@@ -3,7 +3,8 @@ package de.adesso.testservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicLong
 
@@ -21,5 +22,5 @@ class GreetingsController {
 
     @GetMapping("/greeting")
     fun greeting()
-            = arrayOf(counter.incrementAndGet(), "Hello", "from", "spring", "service", "2", "now", "it", "is", LocalDateTime.now().toString())
+            = arrayOf(counter.incrementAndGet(), "Hello", "from", "spring", "service", "1", "now", "it", "is", LocalDateTime.now().toString())
 }
